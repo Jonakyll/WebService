fb2e7cb84d0bcd33c8c81715b712f2ac4768de15
FLambertD <flambert@etud.u-pem.fr>
2020-11-04 15:44:46 +0100

    Banque avec client de test

---
+++ BanqueClient/src/fr/uge/webServices/project/Bank.java
@@ -0,0 +1,15 @@
+/**
+ * Bank.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package fr.uge.webServices.project;
+
+public interface Bank extends java.rmi.Remote {
+    public void display() throws java.rmi.RemoteException;
+    public void addAccount(long id, java.lang.String currency) throws java.rmi.RemoteException;
+    public void depositOf(long id, double amount) throws java.rmi.RemoteException;
+    public void payVehicle(long id, double amountEUR) throws java.rmi.RemoteException;
+}
---
+++ BanqueService/src/fr/uge/webServices/project/Bank.java
@@ -0,0 +1,81 @@
+package fr.uge.webServices.project;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+import javax.xml.rpc.ServiceException;
+
+import com.currencysystem.webservices.currencyserver.CurncsrvReturnRate;
+import com.currencysystem.webservices.currencyserver.CurrencyServerLocator;
+import com.currencysystem.webservices.currencyserver.CurrencyServerSoap;
+
+public class Bank {
+	private HashMap<Long, Account> accountMap;
+	private CurrencyServerSoap soapServer;
+	
+	public Bank() throws ServiceException {
+		this.accountMap =new HashMap<Long, Account>();
+		this.soapServer = new CurrencyServerLocator().getCurrencyServerSoap();
+	}
+	
+	public void addAccount(Long id, String currency) throws RemoteException {
+		if (!soapServer.currencyExists("", currency, true)){
+			System.out.println(soapServer.currencyExists("", currency, true));
+			System.out.println("Compte non ajouté car currency inexistante");
+		}
+		
+		accountMap.put(id, new Account(id, currency));
+		System.out.println("compte "+id+" ajouté Taille de la map: "+accountMap.size());
+	}
+	
+	/*public void addAccount(Long id, Account account) throws RemoteException {
+		if (!soapServer.currencyExists("", account.getCurrency(), true)){
+			System.out.println("Compte non ajouté car currency inexistante");
+		}
+		accountMap.putIfAbsent(id, account);
+	}*/
+
+	private double isAbleToPay(Long id, double amountEUR) throws RemoteException {
+		Account account = accountMap.get(id);
+		double accountCurrentCurrency = (double) soapServer.convert("", "EUR",  account.getCurrency(), account.getValue(), 
+				false, ".", CurncsrvReturnRate.curncsrvReturnRateNumber, "", "");
+		if (account.getValue() >=accountCurrentCurrency) {
+			return accountCurrentCurrency;
+		}
+		return -1;
+	}
+	
+	public void payVehicle(Long id, double amountEUR) throws RemoteException {
+		if (amountEUR<0) {
+			throw new IllegalArgumentException();
+		}
+		double amountToPay = isAbleToPay(id, amountEUR);
+		if (amountToPay==-1) {
+			System.out.println("Achat non effectué, compte pas assez approvisionné");
+			return ;
+		}
+		accountMap.get(id).moneyBackOf(amountToPay);
+		System.out.println("Vehicle acheté");
+		return;
+	}
+	
+	public void depositOf(Long id, double amount) {
+		if (amount<0) {
+			throw new IllegalArgumentException();
+		}
+		accountMap.get(id).moneyDepositOf(amount);
+	}
+
+	public void display() {
+		System.out.println("AFFICHAGE DE LA BANQUE:");
+		System.out.println(accountMap.size());
+		for (Account account : accountMap.values()) {
+			System.out.println(account);
+		}
+	}
+	
+	
+	
+	
+
+}
