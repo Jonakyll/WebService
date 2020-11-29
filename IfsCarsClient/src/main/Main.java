974f85bd637e8caaa3b1788f21a8c6e8c0b72c8a
CHU Jonathan <46070603+Jonakyll@users.noreply.github.com>
2020-11-29 14:27:25 +0100

    update main web client
    création d'un compte bancaire + affichage des modèles des voitures

--- IfsCarsClient/src/main/Main.java
+++ IfsCarsClient/src/main/Main.java
@@ -15,12 +15,21 @@ public class Main {
 		Client client = new Client();
 		Garage garage = new GarageServiceLocator().getGarage();
 
-		client.checkAccount(3L);
+		garage.displayBank();
+
+		System.out.println("===== CLIENT CREATES A BANK ACCOUNT =====");
+		client.createAccount("USD");
+		client.depositOf(1_000_000);
+		System.out.println("id: " + client.getAccount_id());
+		System.out.println("money: " + client.getAmountAccount());
 		
-		System.out.println("==== SHOW CARS TO BUY =====");
+		garage.displayBank();
+
+		System.out.println("\n==== SHOW CARS TO BUY =====");
 		Car[] cars = client.getCars();
 		for (int i = 0; i < cars.length; ++i) {
 			System.out.println("id\t\t" + cars[i].getId());
+			System.out.println("id\t\t" + cars[i].getModel());
 			System.out.println("availability\t" + cars[i].isAvailability());
 			System.out.println("price\t\t" + cars[i].getPrice() + "\n");
 		}
@@ -28,30 +37,29 @@ public class Main {
 		System.out.println("\n===== SHOW CLIENT CART =====");
 		System.out.println("cart:");
 		for (Car car : client.getCart()) {
-			System.out.println("\tid: " + car.getId());
+			System.out.println("" + car.getId() + ": " + car.getModel() + " " + car.getPrice());
 		}
-		System.out.println("amount to pay:\t" + client.getAmountToPay());
+		System.out.println("to pay:\t" + client.getAmountToPay());
 
 		System.out.println("\n===== THE CLIENT ADDS CARS TO HIS CART=====");
 		client.addToCart(0);
-		client.addToCart(1);
-		client.addToCart(2);
-		client.addToCart(3);
+//		client.addToCart(1);
 
 		System.out.println("===== SHOW CLIENT CART =====");
 		System.out.println("cart:");
 		for (Car car : client.getCart()) {
-			System.out.println("\tid: " + car.getId());
+			System.out.println("" + car.getId() + ": " + car.getModel());
 		}
 		System.out.println("amount to pay (in EUR):\t" + client.getAmountToPay());
-		
-		System.out.println("===== THE CLIENT PAYS =====");
+
+		System.out.println("\n===== THE CLIENT PAYS =====");
 		if (client.buy()) {
 			System.out.println("===== CARS PAID =====");
 		} else {
-			System.out.println("===== THE CLIENT COULDN'T PAY =====");			
+			System.out.println("===== THE CLIENT COULDN'T PAY =====");
 		}
-		
-		
+
+		garage.displayBank();
+
 	}
 }
